473de719dbf5e2f91a462247f4d5d60dd8b64ba4
DigitalCrafting <k.kasprzyk2@gmail.com>
2023-08-29 18:50:29 +0200

    Building interpreter - self-evaluating expressions

---
+++ eva.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+
+/**
+ * Eva interpreter
+ * */
+class Eva {
+    eval(exp) {
+        if (isNumber(exp)) {
+            return exp;
+        }
+
+        if (isString(exp)) {
+            return exp.slice(1, -1);
+        }
+
+        if (exp[0] === '+') {
+            const firstOperand = this.eval(exp[1]);
+            const secondOperand = this.eval(exp[2]);
+            return firstOperand + secondOperand;
+        }
+
+        throw 'Unimplemented';
+    }
+}
+
+function isNumber(exp) {
+    return typeof exp === 'number';
+}
+
+function isString(exp) {
+    return typeof exp === 'string' && exp[0] === '"' && exp[exp.length - 1] === '"';
+}
+
+// -------------------------------------------------------------
+// tests
+const eva = new Eva();
+
+assert.strictEqual(eva.eval(1), 1);
+assert.strictEqual(eva.eval('"hello"'), 'hello');
+assert.strictEqual(eva.eval(['+', 1, 5]), 6);
+assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10);
+
+console.log("All assertions passed!");
