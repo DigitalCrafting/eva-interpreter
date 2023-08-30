77acbfd9ac6a61191520b4017d90756105408b69
DigitalCrafting <k.kasprzyk2@gmail.com>
2023-08-30 08:44:48 +0200

    Building interpreter - math operations

--- eva.js
+++ eva.js
@@ -4,7 +4,15 @@ const assert = require('assert');
  * Eva interpreter
  * */
 class Eva {
+    /*
+    * Exp ::= Number
+    * | String
+    * | [+ Exp Exp]
+    * ;
+    * */
     eval(exp) {
+        // -------------------------------------------------------------
+        // Self-evaluating expressions
         if (isNumber(exp)) {
             return exp;
         }
@@ -13,10 +21,26 @@ class Eva {
             return exp.slice(1, -1);
         }
 
+        // -------------------------------------------------------------
+        // Math operations
         if (exp[0] === '+') {
-            const firstOperand = this.eval(exp[1]);
-            const secondOperand = this.eval(exp[2]);
-            return firstOperand + secondOperand;
+            return this.eval(exp[1]) + this.eval(exp[2]);
+        }
+
+        if (exp[0] === '-') {
+            return this.eval(exp[1]) - this.eval(exp[2]);
+        }
+
+        if (exp[0] === '*') {
+            return this.eval(exp[1]) * this.eval(exp[2]);
+        }
+
+        if (exp[0] === '/') {
+            return this.eval(exp[1]) / this.eval(exp[2]);
+        }
+
+        if (exp[0] === '%') {
+            return this.eval(exp[1]) % this.eval(exp[2]);
         }
 
         throw 'Unimplemented';
@@ -35,9 +59,28 @@ function isString(exp) {
 // tests
 const eva = new Eva();
 
+// -------------------------------------------------------------
+// Self-evaluating expressions
 assert.strictEqual(eva.eval(1), 1);
 assert.strictEqual(eva.eval('"hello"'), 'hello');
+
+// -------------------------------------------------------------
+// Math operations
+// Addition
 assert.strictEqual(eva.eval(['+', 1, 5]), 6);
 assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10);
+// Subtraction
+assert.strictEqual(eva.eval(['-', 1, 5]), -4);
+assert.strictEqual(eva.eval(['+', ['-', 3, 2], 5]), 6);
+// Multiplication
+assert.strictEqual(eva.eval(['*', 2, 5]), 10);
+assert.strictEqual(eva.eval(['+', ['*', 3, 2], 5]), 11);
+// Division
+assert.strictEqual(eva.eval(['/', 4, 2]), 2);
+assert.strictEqual(eva.eval(['+', ['/', 4, 2], 5]), 7);
+// Modulo
+assert.strictEqual(eva.eval(['%', 4, 2]), 0);
+assert.strictEqual(eva.eval(['%', 5, 2]), 1);
+assert.strictEqual(eva.eval(['+', ['%', 5, 2], 5]), 6);
 
 console.log("All assertions passed!");
